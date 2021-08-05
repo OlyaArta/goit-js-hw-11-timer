6faa8e5cfbc6960c5d1dd88b141183e347a08b80
OlyaArta <artanika8@gmail.com>
2021-08-05 20:45:00 +0300

    fixed some things

--- js/main-timer.js
+++ js/main-timer.js
@@ -1,11 +1,11 @@
 class Timer {
   constructor() {
       this.watch = null;
-      this.targetDate = new Date('Jul 17, 2023');
-      this.sec = document.querySelector(".secs");
-      this.min = document.querySelector(".mins");
-      this.hour = document.querySelector(".hour");
-      this.day = document.querySelector(".day");
+      this.target = new Date('Jul 17, 2023');
+      this.sec = document.querySelector('[data-value="secs"]');
+      this.min = document.querySelector('[data-value="mins"]');
+      this.hour = document.querySelector('[data-value="hour"]');
+      this.day = document.querySelector('[data-value="day"]');
 
       this.action = this.action.bind(this);
       this.start = this.start.bind(this);
@@ -13,11 +13,11 @@ class Timer {
   }  
 
   action() {
-      const timerId = this.targetDate - new Date();
-      const sec = Math.floor((timer % (1000 * 60)) / 1000);
-      const min = Math.floor((timer % (1000 * 60 * 60)) / (1000 * 60));
-      const hour = Math.floor((timer % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const day = Math.floor(timer / (1000 * 60 * 60 * 24));
+      const timerId = this.target - new Date();
+      const sec = Math.floor((timerId % (1000 * 60)) / 1000);
+      const min = Math.floor((timerId % (1000 * 60 * 60)) / (1000 * 60));
+      const hour = Math.floor((timerId % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const day = Math.floor(timerId / (1000 * 60 * 60 * 24));
 
     this.sec.textContent = sec;
     this.min.textContent = min;
